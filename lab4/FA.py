494245797d4c312c3901a633de275eb40d537628
Puica Mihai <mihaipuica1@gmail.com>
2020-11-12 19:31:43 +0200

    finished lab 4

--- lab4/FA.py
+++ lab4/FA.py
@@ -1,10 +1,12 @@
 class FA:
     def __init__(self, fileName):
+        # sequence = a sequence that has to be checked
         # Q = finite set of states
         # E = finite alphabet
         # F = set of final states
         # q0 = initial state
         # D(elta) = transition function
+        self.__sequence = None
         self.__Q = None
         self.__E = None
         self.__F = None
@@ -18,6 +20,7 @@ class FA:
             # the first strip() gets rid of the newline at the end of the line
             # the split('=')[1] splits the line in the lhs and rhs and only keeps the rhs(states or alphabet)
             # the split(',') splits the states/alphabet into a separate list
+            self.__sequence = file.readline().strip()
             self.__Q = file.readline().strip().split('=')[1].split(',')
             self.__E = file.readline().strip().split('=')[1].split(',')
             self.__F = file.readline().strip().split('=')[1].split(',')
@@ -38,6 +41,8 @@ class FA:
         print("3. Print the set of final states")
         print("4. Print the initial state")
         print("5. Print the transitions")
+        print("6. Check sequence")
+        print("7. Validate FA")
         while True:
             menuCommand = input()
             if menuCommand == "1":
@@ -53,6 +58,9 @@ class FA:
                 for triple in self.__D:
                     transition = "(" + triple[0] + "," + triple[1] + ")=>" + triple[2]
                     print(transition)
+            elif menuCommand == "6":
+                self.checkSequence()
+
 
     def getQ(self):
         return self.__Q
@@ -68,3 +76,45 @@ class FA:
 
     def getD(self):
         return self.__D
+
+    def validateFA(self):
+        valid = True
+        if self.__q0 in self.__Q:
+            valid = valid and True
+
+        for state in self.__F:
+            if state in self.__Q:
+                valid = valid and True
+
+        for transition in self.__Q:
+            if transition[0] in self.__Q and transition[2] in self.__Q and transition[1] in self.__E:
+                valid = valid and True
+
+        if valid is True:
+            print("FA is valid")
+        else:
+            print("FA is not valid")
+
+
+    def checkSequence(self):
+        print("We check if the sequence: " + self.__sequence + " is accepted by the FA")
+        setOfStates = self.__Q
+        state = self.__q0[0]
+        derivations, result = state, state
+        for i in range(len(self.__sequence) - 1):
+            for transition in self.__D:
+
+                if transition[0] == state and transition[2] == self.__sequence[i+1]:
+                    state = transition[2]
+                    derivations = str(derivations) + " =" + str(transition[1]) + "> " + str(state)
+                    result = str(result) + str(state)
+                    break
+            #print('-----')
+
+
+        if result == self.__sequence and result[len(result)-1] in self.__F:
+            print("The sequence can be formed with the following derivations:")
+            print(derivations)
+        else:
+            print("The sequence is not accepted by the FA")
+
